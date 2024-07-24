5e2cf23bc456abe479cb3f379a96557a0a0e7111
ASHill11 <ASHill11@me.com>
2024-07-24 15:55:42 -0500

    modified:   Backend/pokemon.py     - Continuing to add attributes to the Pokemon class

--- Backend/pokemon.py
+++ Backend/pokemon.py
@@ -4,7 +4,7 @@ class Pokemon:
     _instance_count_ = 0
 
     
-    def __init__(self, name, cp, attack, defense, health):
+    def __init__(self, number, name, cp, attack, defense, health):
         # Required Attributes
 
         # Generate a unique ID for each instance of this class, as you can have many of the same Pokemon.
@@ -12,13 +12,14 @@ class Pokemon:
         self.id = Pokemon._instance_count_
 
         # sent over by the web UI
+        self.number = number        # in Pokedex
         self.name = name
         self.cp = cp
 
         # You can derive many more attributes from these 
-        self.attack = attack        # Integer, 0-15
-        self.defense = defense      # Integer, 0-15
-        self.health = health        # Integer, 0-15
+        self.attack = attack        # integer, 0-15
+        self.defense = defense      # integer, 0-15
+        self.health = health        # integer, 0-15
         # The search '(N)attack' will return Pokemon with the matching attack appraisal (int 0-4)
         # The search 'attack(N) will return Pokemon with the matching attack stat (int 0-15)
 
@@ -28,27 +29,35 @@ class Pokemon:
 
     # Due to the game not not meaningfully differentiating between the HP IVs and HP (hit points), I have had to choose to call one 'HP' and the other 'health'
     # This is hit points
-    HP = None
+    nickname = None         # string: 'nickname'
+    HP = None               # integer: X
+    sex = None              # string: male, female, or None
+    region = None           # string: <REGION>
+    year = None             # integer: 20XX
 
-    type1 = None
-    type2 = None
+    type1 = None            # string: <TYPE>
+    type2 = None            # string: <TYPE> OR None
 
-    fast_move = None
-    charged_move1 = None
-    charged_move2 = None
+    fast_move = None        # string: 'name'
+    charged_move1 = None    # string: 'name'
+    charged_move2 = None    # string: 'name' OR None
 
-    sex = None
+    weight = None           # float: x.xx (kg)
+    height = None           # float: x.xx (m)
+    classification = None   # string: 'XXL', 'XL', 'XS', or 'XXS'
 
-    weight = None
-    height = None
+    tags = []   # Items in list defined by user, should all be stored as strings
 
-    tags = []
+    # NOTE: Currently no way of knowing these values without significant trial and error on the user's end in the first place.
+    # Not to mention, that the program would need to age the Pokemon stored in it to maintain accuracy.
+    age = None              # integer: N (days)
+    # Distance is calculated from present location, meaning this may well be useless to add have.
+    # For now, it is here as a placeholder to be sure I haven't missed anything.
+    distance = None         # integer: N (km)
 
 
     # Binary attributes
     # 1 is yes, 0 is no
-
-    # Innate qualities
     _favorite_ = 0      # Favorited?
     _tagged_ = 0        # Tagged NOTE: In game search uses '#' for this filter
 
@@ -67,6 +76,9 @@ class Pokemon:
 
     _traded_ = 0        # Traded?
     _lucky_ = 0         # Lucky?
+
+    _item_ = 0          # Evolves with item?
+    _tradeevolve_ = 0   # Evolves with trade?
 
 
     
