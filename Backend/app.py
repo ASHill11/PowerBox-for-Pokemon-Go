a861ab675e77a93811586ac151bb13839e6cc7f7
ASHill11 <ASHill11@me.com>
2024-07-24 13:26:31 -0500

    Just storyboarding
    Getting a feel for custom classes

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,11 @@
-# This will be the main Python file which interfaces with other Python files, as well as being a hub for APIs
+
+
+"""
+This will be the main Python file which interfaces with other Python files, as well as being a hub for APIs
+
+TODO:
+- Add Pokedex
+    - Consider whether to tie candy to the Pokedex or some other store
+- Add other global resources, such as stardust
+- 
+"""  
--- Backend/scratch.py
+++ Backend/scratch.py
@@ -1,15 +1,24 @@
 
+
 class Pokemon:
-    # Required Attributes
+    _instance_count_ = 0
+
+    
     def __init__(self, name, cp):
+        # Required Attributes
+
+        # Generate a unique ID for each instance of this class, as you can have many of the same Pokemon.
+        Pokemon._instance_count_ += 1
+        self.id = Pokemon._instance_count_
+        # sent over by the web UI
         self.name = name
         self.cp = cp
 
     
 
-    attack = None
-    health = None
-    defense = None
+    _attack_ = None
+    _health_ = None
+    _defense_ = None
 
 
 x = Pokemon(input('Enter name: '), input('Enter CP: '))
