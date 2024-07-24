131fb481bfc01efc52be8d0f6472e0490deb37da
ASHill11 <ASHill11@me.com>
2024-07-24 16:14:20 -0500

    modified:   Backend/app.py

--- Backend/app.py
+++ Backend/app.py
@@ -6,6 +6,6 @@ This will be the main Python file which interfaces with other Python files, as w
 TODO:
 - Add Pokedex
     - Consider whether to tie candy to the Pokedex or some other store
+    - Mega energy, as well
 - Add other global resources, such as stardust
-- 
 """  
