967619c2ade1296aa72414f5a5d9069e49fabe09
ASHill11 <ASHill11@me.com>
2024-07-25 09:59:29 -0500

    modified:   Backend/app.py modified:   Frontend/index.html
    As of this moment, the HTML page, when hosted locally, can now communicate with the localhost.

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,30 @@
 
 
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import sys
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def index():
     return('Howdy')
+
+# GPT
+@app.route('/api/input', methods=['POST'])
+def process_input():
+    data = request.json
+    if 'input_value' in data:
+        input_value = data['input_value']
+        if isinstance(input_value, int):
+            print(f'Input received! >{input_value}<')
+
+            return jsonify({'message': f'Input value received: {input_value}'}), 200
+        else:
+            return jsonify({'error': 'Input value must be an integer'}), 400
+    return jsonify({'error': 'Input value not provided'}), 400
 
 """
 This will be the main Python file which interfaces with other Python files, as well as being a hub for APIs
@@ -26,4 +42,4 @@ TODO:
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=4000)
+    app.run(host="0.0.0.0", port=4000, debug=True)
